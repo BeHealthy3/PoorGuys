8df8aa8353ad43616239c394124d2c1ffb86d4fc
KSYong <ericyong95@gmail.com>
2023-05-16 01:36:27 +0900

    [feat] : 초기 프로젝트 생성

---
+++ PoorGuys/PoorGuysApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoorGuysApp.swift
+//  PoorGuys
+//
+//  Created by 권승용 on 2023/05/16.
+//
+
+import SwiftUI
+
+@main
+struct PoorGuysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PoorGuysUITests/PoorGuysUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  PoorGuysUITestsLaunchTests.swift
+//  PoorGuysUITests
+//
+//  Created by 권승용 on 2023/05/16.
+//
+
+import XCTest
+
+final class PoorGuysUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
