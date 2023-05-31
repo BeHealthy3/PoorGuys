b11a6ac9699e2fc230c2d9cb37cadfbc83d96e38
KSYong <ericyong95@gmail.com>
2023-05-31 17:29:04 +0900

    [feat] View에 if 익스텐션 추가

---
+++ PoorGuys/Utilities/Extensions/View+Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  View+Extensions.swift
+//  PoorGuys
+//
+//  Created by 권승용 on 2023/05/31.
+//
+
+import SwiftUI
+
+extension View {
+    
+    @ViewBuilder func `if`<Content: View>(_ condition: Bool, transform: (Self) -> Content) -> some View {
+        if condition {
+            transform(self)
+        } else {
+            self
+        }
+    }
+    
+}
