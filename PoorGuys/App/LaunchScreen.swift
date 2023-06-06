6ce58d674e3f8769e9752f0d4de9884766beee72
KSYong <ericyong95@gmail.com>
2023-06-07 03:42:15 +0900

    [feat] : 런치스크린 구현 / 런치스크린 보이는 동안 로그인 뷰 보일지 / 메인 뷰 보일지 결정

---
+++ PoorGuys/App/LaunchScreen.swift
@@ -0,0 +1,21 @@
+//
+//  LaunchScreen.swift
+//  PoorGuys
+//
+//  Created by 권승용 on 2023/06/06.
+//
+
+import SwiftUI
+
+struct LaunchScreen: View {
+    var body: some View {
+        Text("어푸어푸!")
+            .font(.largeTitle)
+    }
+}
+
+struct LaunchScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchScreen()
+    }
+}
