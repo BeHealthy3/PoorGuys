06658531d598663f17bc9c23423f8f617a7583fb
ThisidEhd <sem789456@gmail.com>
2023-07-15 16:39:56 +0900

    feat: swipe to go back

---
+++ PoorGuys/Utilities/Extensions/UINavigationController+Extensions.swift
@@ -0,0 +1,19 @@
+//
+//  UINavigationController+Extensions.swift
+//  PoorGuys
+//
+//  Created by 신동훈 on 2023/07/15.
+//
+
+import Foundation
+
+extension UINavigationController: ObservableObject, UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
